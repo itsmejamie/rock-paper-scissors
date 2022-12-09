bdc013053e07cd0db30a4397ebdbf9b3a542a61a
Jamie Fraser <jamiefraser87@hotmail.com>
2022-12-09 18:10:20 +0000

    Updated code for UI

--- lets-play.js
+++ lets-play.js
@@ -1,9 +1,24 @@
-console.log ("Hello World!")
-
 playerScore = 0
 computerScore = 0
 
-//Computer makes a random choice between rock, paper and scissors//
+// creates HTML for player and computer score and also 'Make your selection'
+const body = document.body;
+const div = document.createElement('div');
+const div2 = document.createElement('div2');
+const div4 = document.createElement('div4')
+body.append(div4)
+body.append(div)
+body.append(div2)
+
+div4.innerHTML = '<h1>Make your selection</h1>'
+
+//Updates HTML with Player and Computer Score
+function updateScore (){
+    div.innerText = 'Player Score: ' + playerScore;
+    div2.innerText = 'Computer Score: ' + computerScore;
+}
+
+//Computer makes a random choice between rock, paper and scissors
 var getComputerChoice = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 };
@@ -14,53 +29,101 @@ let arr = [
     "Scissors",
 ];
 
-// Logic behind the games and includes a score counter //
+// Each click of the button plays a round of Rock, Paper, Scissors
+const btn = document.querySelector("#rock")
+btn.addEventListener('click', () => {
+    playerChoice = "Rock";
+    computerChoice = getComputerChoice(arr);
+    playRound(playerChoice, computerChoice);
+    updateScore()
+    winner(playerScore, computerScore)
+} ) 
+
+const btn2 = document.querySelector("#paper")
+btn2.addEventListener('click', () => {
+    playerChoice = "Paper";
+    computerChoice = getComputerChoice(arr);
+    playRound(playerChoice, computerChoice);
+    updateScore()
+    winner(playerScore, computerScore)
+} ) 
+
+const btn3 = document.querySelector("#scissors")
+btn3.addEventListener('click', () => {
+    playerChoice = "Scissors";
+    computerChoice = getComputerChoice(arr);
+    playRound(playerChoice, computerChoice);
+    updateScore()
+    winner(playerScore, computerScore)
+} ) 
+
+ // Logic behind the games//
 function playRound(playerChoice, computerChoice) {
     if (playerChoice == "Rock" && computerChoice == "Scissors")
-        return confirm('You win! ' + playerChoice + ' beats ' + computerChoice) +
-        playerScore ++;
+        {playerScore++;    
+        div4.innerText = 'You win! ' + playerChoice + ' beats ' + computerChoice;}
     else if (playerChoice == "Rock" && computerChoice == "Paper") 
-        return confirm('You lose! ' + computerChoice + ' beats ' + playerChoice) +
-        computerScore++;
+        {computerScore++;
+        div4.innerText =`You lose! ${computerChoice} beats ${playerChoice}`;}
     else if (playerChoice == computerChoice)
-        return confirm('You tied! Both players chose ' + computerChoice) 
+        div4.innerText ='You tied! Both players chose ' + computerChoice; 
     else if (playerChoice == "Scissors" && computerChoice == "Paper") 
-        return confirm('You win! ' + playerChoice + ' beats ' + computerChoice) +
-        playerScore ++;
+        {playerScore++;    
+        div4.innerText ='You win! ' + playerChoice + ' beats ' + computerChoice}
     else if (playerChoice == "Scissors" && computerChoice == "Rock")
-        return confirm('You lose! ' + computerChoice + ' beats ' + playerChoice) +
-        computerScore ++;
+        {computerScore ++;
+        div4.innerText ='You lose! ' + computerChoice + ' beats ' + playerChoice}
     else if (playerChoice == "Paper" && computerChoice == "Rock")
-        return confirm('You win! ' + playerChoice + ' beats ' + computerChoice) +
-        playerScore ++;
-     else if (playerChoice == "Paper" && computerChoice == "Scissors")
-        return confirm('You lose! ' + computerChoice + ' beats ' + playerChoice) +
-        computerScore ++;
-     
+        {playerScore ++;
+        div4.innerText ='You win! ' + playerChoice + ' beats ' + computerChoice}
+    else if (playerChoice == "Paper" && computerChoice == "Scissors")
+        {computerScore ++;
+        div4.innerText ='You lose! ' + computerChoice + ' beats ' + playerChoice}
 }
 
+//keeps track of score and resets at 5
+function winner(playerScore, computerScore) {
+    if (playerScore == 5) {
+        alert ('You win!') + reset();       
+    }
+    else if (computerScore == 5) {
+        alert ('Game Over! You lose!') + reset()
+    }
+}
+
+//
+function reset () {
+    playerScore = 0;
+    computerScore = 0;
+    updateScore()
+    div4.innerHTML = '<h1>Make your selection</h1>'
+
+}
+
+//CODE FROM NON UI VERSION
 // Function to keep the score of the game //
-function keepScore (playerScore, computerScore) {
+/* function keepScore (playerScore, computerScore) {
     if (playerScore>computerScore)
     return confirm ('Congratulation, you won with ' + playerScore + ' points');
     else if (playerScore<computerScore)
     return confirm ('You lost. The computer was smarter than you');
     else if (playerScore==computerScore)
     return confirm ('You both tied')
+    
 }
-keepScore();
-
+keepScore();  */
 
 //Function to play 5 rounds of rock, paper or scissors before announcing the winner at the end//
-function game (){
+
+/*function game (){
     for (let i = 0; i < 5; i++) {
-    playerChoice = prompt ("Rock, paper or scissors?");
+    playerChoice = ????
     computerChoice = getComputerChoice(arr);
     playRound (playerChoice, computerChoice);
     console.log(computerChoice);
     console.log(playerChoice);
-    console.log (playerScore, computerScore);}
+    updateScore()
     keepScore(playerScore, computerScore);
 }
-game();
-
+}
+game(); */
